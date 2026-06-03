d1848ea1f293e4d538e476f43c237412fa6fc969
sureshalluru <sureshalluru@gmail.com>
2026-06-03 16:44:08 -0500

    address fix

--- apps/customer/src/Pages/LaundryPickupPage.jsx
+++ apps/customer/src/Pages/LaundryPickupPage.jsx
@@ -93,7 +93,7 @@ export default function LaundryPickupPage({laundryId,customerId,customerPaymentI
     const [address, setAddress] = useState(localStorage.getItem('customerAddress') || ''); // Store the user address
     const [addressInstructions,setAddressInstructions] = useState(''); // Store the address instructions
     const [doorNumber, setDoorNumber] = useState('');// Store the Address Door Number
-    const [isAddressValidated, setIsAddressValidated] = useState(false); // Track if the address is validated
+    const [isAddressValidated, setIsAddressValidated] = useState(!!localStorage.getItem('customerAddress')); // Auto-validate if address exists
 
     // For Google Maps API
     const searchBoxRef = useRef(null);
